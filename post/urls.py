5671ee0d4f51a9e96a5104cbdadde39715b1d020
a-onode <akt.onodera@gmail.com>
2025-03-22 20:07:21 +0900

    ログアウト機能を実装

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import signup, signin, index
+from .views import signup, signin, signout, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
+    path('signout/', signout, name='signout'),
     path('index/', index, name='index'),
 ]
