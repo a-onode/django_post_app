e809c5e6aeba40663a01e64a655c9f88cc611b68
a-onode <akt.onodera@gmail.com>
2025-03-26 18:00:06 +0900

    静的ファイルの実装

--- postproject/urls.py
+++ postproject/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', include('post.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
