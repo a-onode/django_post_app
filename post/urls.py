3eaa2ebebd4b43437ca93bc3b44d7d92d7f6ea74
a-onode <akt.onodera@gmail.com>
2025-03-26 16:52:21 +0900

    ディレクトリ構成の修正

--- post/urls.py
+++ post/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path
 
-from .views import (
-    signup,
-    signin,
-    signout,
-    index,
-    create,
-    detail,
-    update,
-    delete
-)
+from .views import index, create, detail, update, delete
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('signin/', signin, name='signin'),
-    path('signout/', signout, name='signout'),
     path('index/', index, name='index'),
     path('create/', create, name='create'),
     path('detail/<int:id>/', detail, name='detail'),
